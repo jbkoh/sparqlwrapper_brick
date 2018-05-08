af122e58a89646d5528d8e428f3f476f14805bbc
Jason Koh <bk7749@gmail.com>
2018-05-08 00:32:40 -0700

    update

--- brick_endpoint.py
+++ brick_endpoint.py
@@ -1,8 +1,9 @@
 import rdflib
-from rdflib import RDFS, RDF, OWL
-from SPARQLWrapper import SPARQLWrapper, JSON, SELECT, DIGEST
-from SPARQLWrapper import JSON, SELECT, DIGEST, GET, POST
-from rdflib import URIRef
+from rdflib import RDFS, RDF, OWL, Namespace
+from rdflib.namespace import FOAF
+from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import JSON, SELECT, INSERT, DIGEST, GET, POST
+from rdflib import URIRef, Literal
 
 from copy import deepcopy
 
@@ -10,6 +11,7 @@ import pdb
 
 
 class BrickEndpoint(object):
+
     def __init__(self, sparql_url, brick_version, base_ns=''):
         self.BRICK_VERSION = brick_version
         self.sparql_url = sparql_url
@@ -20,28 +22,45 @@ class BrickEndpoint(object):
         self.sparql.setHTTPAuth(DIGEST)
         if not base_ns:
             base_ns = 'http://example.com/'
-        self.BASE = base_ns
+        self.BASE = Namespace(base_ns)
         self.base_graph = base_ns[:-1]
         self.sparql.addDefaultGraph(self.base_graph)
-        self.BRICK = 'https://brickschema.org/schema/{0}/Brick#'\
-            .format(self.BRICK_VERSION)
-        self.BRICK_USE = 'https://brickschema.org/schema/{0}/BrickUse#'\
-            .format(self.BRICK_VERSION)
-        self.BF = 'https://brickschema.org/schema/{0}/BrickFrame#'\
-            .format(self.BRICK_VERSION)
-        self.BRICK_TAG = 'https://brickschema.org/schema/{0}/BrickTag#'\
-            .format(self.BRICK_VERSION)
+        self.BRICK = Namespace(
+            'https://brickschema.org/schema/{0}/Brick#'\
+            .format(self.BRICK_VERSION))
+        self.BRICK_USE = Namespace(
+            'https://brickschema.org/schema/{0}/BrickUse#'\
+            .format(self.BRICK_VERSION))
+        self.BF = Namespace(
+            'https://brickschema.org/schema/{0}/BrickFrame#'\
+            .format(self.BRICK_VERSION))
+        self.BRICK_TAG = Namespace(
+            'https://brickschema.org/schema/{0}/BrickTag#'\
+            .format(self.BRICK_VERSION))
 
-        self.q_prefix = """
-            prefix brick: <{0}>
-            prefix bf: <{1}>
-            prefix brick-tag: <{2}>
-            prefix brick-use: <{3}>
-            prefix rdfs: <{4}>
-            prefix rdf: <{5}>
-            prefix owl: <{6}>
-	""".format(self.BRICK, self.BF, self.BRICK_TAG, self.BRICK_USE,
-                   str(RDFS), str(RDF), str(OWL))
+        self.namespaces = {
+            '': self.BASE,
+            'brick':self.BRICK,
+            'bf': self.BF,
+            'brick_tag': self.BRICK_TAG,
+            'brick_use': self.BRICK_USE,
+            'rdfs': RDFS,
+            'rdf': RDF,
+            'owl': OWL,
+            'foaf': FOAF
+        }
+        #self.q_prefix = '\n'.join([
+        #    'prefix {0}: {1}'.format(prefix, ns.uri.n3()) for prefix, ns
+        #    in self.namespaces.items()]) + '\n'
+        self.q_prefix = ''
+        for prefix, ns in self.namespaces.items():
+            if 'uri' in dir(ns):
+                ns_n3 = ns.uri.n3()
+            else:
+                ns_n3 = ns[''].n3()
+
+            self.q_prefix += 'prefix {0}: {1}\n'.format(prefix, ns_n3)
+        self.q_prefix += '\n'
 
         self._init_brick_constants()
 
@@ -52,10 +71,29 @@ class BrickEndpoint(object):
         # If need to optimize accessing sparql object.
         return self.sparql
 
-    def exec_update(self, qstr):
-        return self.exec_query(qstr, is_update=True)
+    def update(self, qstr):
+        return self.query(qstr, is_update=True)
 
-    def exec_query(self, qstr, is_update=False):
+    def _format_select_res(self, raw_res):
+        var_names = raw_res['head']['vars']
+        try:
+            values = [[row[var_name]['value'] for var_name in var_names]
+                  for row in raw_res['results']['bindings']]
+        except:
+            pdb.set_trace()
+        #TODO: Below line is a hack.
+        var_names = ['?'+var_name for var_name in var_names]
+        return [var_names, values]
+
+    def parse_result(self, res):
+        raw_res = res
+        common_res = res
+        return common_res, raw_res
+
+    def raw_query(self, qstr):
+        return self.query(qstr)
+
+    def query(self, qstr, is_update=False):
         sparql = self._get_sparql()
         if is_update:
             sparql.setMethod(POST)
@@ -65,9 +103,13 @@ class BrickEndpoint(object):
         qstr = self.q_prefix + qstr
         sparql.setHTTPAuth
         sparql.setQuery(qstr)
-        res = sparql.query().convert()
+        raw_res = sparql.query().convert()
         if sparql.queryType == SELECT:
-            res = res['results']['bindings']
+            res = self._format_select_res(raw_res)
+        elif sparql.queryType == INSERT:
+            res = raw_res # TODO: Error handling here
+        elif sparql.queryType == 'LOAD':
+            res = raw_res # TODO: Error handling here
         return res
 
     def _create_insert_query(self, triples):
@@ -83,21 +125,87 @@ class BrickEndpoint(object):
             """
         return q
 
-    def add_instance(self, entity_name, tagset):
+    def _is_bool(self, s):
+        s = s.lower()
+        if s == 'true' or s == 'false':
+            return True
+        else:
+            return False
+
+    def _str2bool(self, s):
+        s = s.lower()
+        if s == 'true':
+            return True
+        elif s == 'false':
+            return False
+        else:
+            raise Exception('{0} is not convertible to boolean'.format(s))
+
+    def _is_float(self, s):
+        try:
+            float(s)
+            return True
+        except:
+            return False
+
+    def _parse_term(self, term):
+        if isinstance(term, URIRef) or isinstance(term, Literal):
+            return term
+        elif isinstance(term, str):
+            if 'http' == term[0:4]:
+                node = URIRef(term)
+            elif ':' in term: #TODO: This condition is dangerous.
+                [ns, id_] = term.split(':')
+                ns = self.namespaces[ns]
+                node = ns[id_]
+            else:
+                if term.isdigit():
+                    term = int(term)
+                elif self._is_float(term):
+                    term = float(term)
+                if self._is_bool(term):
+                    term = _str2bool(term)
+                node = Literal(term)
+        else:
+            node = Literal(term)
+        return node
+
+    def add_triple(self, pseudo_s, pseudo_p, pseudo_o):
+        triple = self.make_triple(pseudo_s, pseudo_p, pseudo_o)
+        self.add_triples([triple])
+
+    def make_triple(self, pseudo_s, pseudo_p, pseudo_o):
+        s = self._parse_term(pseudo_s)
+        p = self._parse_term(pseudo_p)
+        o = self._parse_term(pseudo_o)
+        return (s, p, o)
+
+
+    def add_triples(self, pseudo_triples):
+        triples = [self.make_triple(*pseudo_triple)
+                   for pseudo_triple in pseudo_triples]
+        self._add_triples(triples)
+
+    def _add_triples(self, triples):
+        q = self._create_insert_query(triples)
+        res = self.update(q)
+
+
+    def add_brick_instance(self, entity_name, tagset):
         entity = URIRef(self.BASE + entity_name)
         tagset = URIRef(self.BRICK + tagset)
         triples = [(entity, RDF.type, tagset)]
-        q = self._create_insert_query(triples)
-        res = self.exec_update(q)
+        self._add_triples(triples)
+        return str(entity)
 
     def load_schema(self):
-        schema_urls = [ns[:-1] + '.ttl' for ns in
+        schema_urls = [str(ns)[:-1] + '.ttl' for ns in
                        [self.BRICK, self.BRICK_USE, self.BF, self.BRICK_TAG]]
         load_query_template = 'LOAD <{0}> into <{1}>'
         for schema_url in schema_urls:
             qstr = load_query_template.format(
                 schema_url.replace('https', 'http'), self.base_graph)
-            res = self.exec_update(qstr)
+            res = self.update(qstr)
 
 
 if __name__ == '__main__':
@@ -108,4 +216,5 @@ if __name__ == '__main__':
         ?s rdfs:subClassOf+ brick:Temperature_Sensor .
         }
         """
-    res = endpoint.exec_query(test_qstr)
+    res = endpoint.query(test_qstr)
+    print(res)
